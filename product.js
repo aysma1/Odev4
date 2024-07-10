efdf908449070a6ffa10eb6fa9208e39e7e65868
aysma1 <168637232+aysma1@users.noreply.github.com>
2024-07-11 00:07:32 +0300

    Initial commit

---
+++ product.js
@@ -0,0 +1,218 @@
+    let productList = [
+        {
+            "ProductId": 1,
+            "ProductName": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+            "ProductPuan": 4,
+            "ProductReviewCount": 120,
+            "ProductPrice": "109.95$",
+            "ProductImage": "product.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 2,
+            "ProductName": "Mens Casual Premium Slim Fit T-Shirts",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 259,
+            "ProductPrice": "22.3$",
+            "ProductImage": "product2.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 3,
+            "ProductName": "Mens Cotton Jacket",
+            "ProductPuan": 4,
+            "ProductReviewCount": 500,
+            "ProductPrice": "55.99$",
+            "ProductImage": "product3.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 4,
+            "ProductName": "Mens Casual Slim Fit",
+            "ProductPuan": 3.5,
+            "ProductReviewCount": 430,
+            "ProductPrice": "15.99$",
+            "ProductImage": "product4.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 5,
+            "ProductName": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 400,
+            "ProductPrice": "695$",
+            "ProductImage": "product5.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 6,
+            "ProductName": "Solid Gold Petite Micropave",
+            "ProductPuan": 4,
+            "ProductReviewCount": 70,
+            "ProductPrice": "168$",
+            "ProductImage": "product6.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 7,
+            "ProductName": "White Gold Plated Princess",
+            "ProductPuan": 2,
+            "ProductReviewCount": 400,
+            "ProductPrice": "9.99$",
+            "ProductImage": "product7.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 8,
+            "ProductName": "Pierced Owl Rose Gold Plated Stainless Steel Double",
+            "ProductPuan": 4,
+            "ProductReviewCount": 100,
+            "ProductPrice": "10.99$",
+            "ProductImage": "product8.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 9,
+            "ProductName": "WD 2TB Elements Portable External Hard Drive - USB 3.0",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 203,
+            "ProductPrice": "64$",
+            "ProductImage": "product9.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 10,
+            "ProductName": "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 470,
+            "ProductPrice": "109$",
+            "ProductImage": "product10.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 11,
+            "ProductName": "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+            "ProductPuan": 5,
+            "ProductReviewCount": 319,
+            "ProductPrice": "109$",
+            "ProductImage": "product11.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 12,
+            "ProductName": "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 400,
+            "ProductPrice": "114$",
+            "ProductImage": "product12.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 13,
+            "ProductName": "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+            "ProductPuan": 4,
+            "ProductReviewCount": 250,
+            "ProductPrice": "599$",
+            "ProductImage": "product13.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 14,
+            "ProductName": "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 140,
+            "ProductPrice": "999.99$",
+            "ProductImage": "product14.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 15,
+            "ProductName": "Sony WH-1000XM4 Wireless Noise-Canceling Headphones",
+            "ProductPuan": 3,
+            "ProductReviewCount": 500,
+            "ProductPrice": "349.99$",
+            "ProductImage": "product15.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 16,
+            "ProductName": "Apple MacBook Pro 16-inch, 16GB RAM, 1TB Storage",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 300,
+            "ProductPrice": "2399.99$",
+            "ProductImage": "product16.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 17,
+            "ProductName": "Google Pixel 6 Pro - 128GB - Unlocked",
+            "ProductPuan": 2,
+            "ProductReviewCount": 150,
+            "ProductPrice": "899.99$",
+            "ProductImage": "product17.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 18,
+            "ProductName": "Microsoft Surface Pro 7 – 12.3\" Touch-Screen – Intel Core i5 – 8GB Memory – 128GB SSD",
+            "ProductPuan": 2,
+            "ProductReviewCount": 200,
+            "ProductPrice": "749.99$",
+            "ProductImage": "product18.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 19,
+            "ProductName": "Bose SoundLink Bluetooth Speaker II",
+            "ProductPuan": 1,
+            "ProductReviewCount": 450,
+            "ProductPrice": "129.99$",
+            "ProductImage": "product19.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 20,
+            "ProductName": "Canon EOS Rebel T7 DSLR Camera with 18-55mm Lens",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 350,
+            "ProductPrice": "449.99$",
+            "ProductImage": "product20.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 21,
+            "ProductName": "Samsung Galaxy Watch 4 44mm Smartwatch",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 600,
+            "ProductPrice": "249.99$",
+            "ProductImage": "product21.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 22,
+            "ProductName": "Nintendo Switch with Neon Blue and Neon Red Joy-Con",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 700,
+            "ProductPrice": "299.99$",
+            "ProductImage": "product22.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 23,
+            "ProductName": "Dyson V11 Torque Drive Cordless Vacuum Cleaner",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 400,
+            "ProductPrice": "599.99$",
+            "ProductImage": "product23.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        },
+        {
+            "ProductId": 24,
+            "ProductName": "GoPro HERO9 Black - Waterproof Action Camera",
+            "ProductPuan": 4.5,
+            "ProductReviewCount": 300,
+            "ProductPrice": "399.99$",
+            "ProductImage": "product24.png",
+            "ProductContentText": "Lorem Ipsum Samet is Berlin, Lorem To In Do To Mıddle Liddle"
+        }
+    ];
